653b68e73878394936825c9d401269e42c7f3f93
Gustavo-404 <gustavogabriel77849@gmail.com>
2021-06-25 17:52:54 -0300

    add TypeORM & sqLite

--- NodeProject/src/server.ts
+++ NodeProject/src/server.ts
@@ -1,25 +1,7 @@
 import express, { request, response } from "express";
+import "reflect-metadata";
+import "./database"
 
-// @type/express
 const app = express();
 
-/**
- * GET    => BUSCAR UMA INFORMAÇÃO
- * POST   => INSERIR UMA INFORMAÇÃO
- * PUT    => ALTERAR UMA INFORMAÇÃO
- * DELETE => DELETAR UMA INFROMAÇÃO
- * PATCH  => ALTERAR UMA INFORMAÇÃO ESPECIFICA (SOMENTE SENHA ... etc)
- */
-
-app.get("/test", (request, response) => {
-    // REQUEST => ENTRANDO
-    // RESPONSE => SAINDO
-    return response.send("OLÁ, MUNDO! /GET")
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("OLÁ, MUNDO! /POST")
-})
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
