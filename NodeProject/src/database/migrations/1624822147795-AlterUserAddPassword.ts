7cbdefc60de08d1387c779f593f14c542bfba93f
Gustavo-404 <gustavogabriel77849@gmail.com>
2021-06-27 16:38:06 -0300

    add jwt dependency & add column password in user

---
+++ NodeProject/src/database/migrations/1624822147795-AlterUserAddPassword.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUserAddPassword1624822147795 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn("users", //Adiciona a migration users ja criada a coluna password
+            new TableColumn({
+                name: "password",
+                type: "varchar"
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users", "password") // caso necessite dar um yarn typeorm migration:revert para reverter
+    }
+
+}
